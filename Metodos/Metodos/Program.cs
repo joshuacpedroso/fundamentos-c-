a68420be6f5ba4319e0ed14fb6d3c66dcd370cfc
Joshua Pedroso <106555455+joshuacpedroso@users.noreply.github.com>
2023-02-14 14:33:17 -0300

    Initial commit

---
+++ Metodos/Metodos/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Metodos {
+    class Program{
+        static void Main(string[] args)
+        {
+            MeuMetodo();
+            string nome = ReturnName("Joshua", "Pedroso", 23);
+            Console.WriteLine(nome);
+        }
+        static void MeuMetodo()
+        {
+            Console.WriteLine("Refazendo novamente fundamentos do C#");
+        }
+        static string ReturnName(
+            string nome, 
+            string sobrenome,
+            int idade)
+        {
+            return nome + " " + sobrenome + " Tem " + idade.ToString();
+        }
+    }
+}
+
---
+++ Types/Types/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
